3ec51fe942e772c911d10fd207dfff768c2c64d6
SteakBarker <sn311706@KLI2017-12286M.kent.k12.wa.us>
2016-03-21 08:29:39 -0700

    Now check if game is filled, and adds them too the game

--- www/html/join.php
+++ www/html/join.php
@@ -9,10 +9,11 @@ function cleanData($data){
 	return htmlentities($data);
 }
 
-if(empty($_GET["id"])){
+if(empty($_GET["id"]) || empty($_GET["name"])){
 	exit("Invalid data");
 }
 $id = cleanData($_GET["id"]);
+$name = cleanData($_GET["name"]);
 
 //This method to read from the DB is ripped from the PHP doc
 
@@ -20,15 +21,13 @@ require_once('../mysql_connect.php');
 $dbc = createDefaultConnection('games');
 
 
-$query = "SELECT * FROM game WHERE id=?";
+$query = "SELECT filled FROM game WHERE id=?";
 $stmt = $dbc->stmt_init();
 
 if(!$stmt->prepare($query))
 {
 	echo "Statement failed to prepare!";
-}
-else
-{
+}else{
 	$stmt->bind_param("s", $id);
 	$stmt->execute();
 	$result = $stmt->get_result();
@@ -36,7 +35,30 @@ else
 	//TODO
 	//This is where we should check if the game is full, and if it's not, allow the user to join
 	
-	echo $result;
+	//NO IDEA IF THIS WILL WORK. NO WAY TO TEST IT RIGHT NOW
+	
+	if($result == '0'){
+		//The game is not filled. We can allow them to join
+		
+		require_once('../createPlayer.php');
+		$player_data = createPlayer($name);
+			//Player data is an arary["id","login_id]
+			
+		//TODO add the new player to the table, then set fileld to true (1)
+		
+		$query = "update game set p1_id=?, filled=1";
+		$stmt = $dbc->stmt_init();
+		if(!$stmt->prepare($query)){
+			echo "Statement failed to prepare!";
+		}else{
+			$stmt->bind_param("s", $player_data["id"]);
+			$worked = $stmt->execute();
+			
+			if($worked){
+				echo "You've been added";
+				//TODO: Create them a link to join
+			}
+		}
 }
 
 $stmt->execute();
