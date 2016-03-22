cad93bd2cc398a4b05a70c2bd74526f27d90e9b1
Matthew <vandogman@gmail.com>
2016-03-21 20:40:50 -0700

    Player now uses one uniqe ID instead of ID and login_ID

--- www/createPlayer.php
+++ www/createPlayer.php
@@ -11,18 +11,38 @@
 			require_once('mysql_connect.php');
 			$dbc = createDefaultConnection('games');
 			
-			$stmt = $dbc->prepare('INSERT INTO player (id, name, seen, login_id) VALUES(NULL, ?, 1, ?)');
-			$login_id = randomString_Alphanumeric(5);
-			$stmt->bind_param('ss', $name, $login_id);
+			$query = "SELECT * FROM player WHERE id=?";
+			$id = 0;
+			$stmt = $dbc->stmt_init();
+			for ($i = 0; $i<=100; $i++) {
+				if(!$stmt->prepare($query)){
+					$dbc->close();
+					exit("Statement failed to prepare!");
+				}
+				$id = randomString_Alphanumeric(5);
+				$stmt->bind_param("s", cleanData_Alphanumeric($id));
+				$stmt->execute();
+
+				$result = $stmt->get_result();
+				$row = $result->fetch_array();
+				if($row === null){
+					//The ID has not been used
+					break;
+				}
+				if($i >= 25){ // We will only give it 25 tries
+					$stmt->close();
+					$dbc->close();
+					exit("Could not find uniqe code");
+				}
+			}
+			
+			$stmt = $dbc->prepare('INSERT INTO player (id, name, seen) VALUES(?, ?, 1)');
+			$stmt->bind_param('ss',$id, $name);
 			
 			$worked = $stmt->execute();
 			
 			if($worked){
-				$id = $dbc->insert_id;
-				
-				$array["id"] = $id;
-				$array["login_id"] = $login_id;
-				return $array;	
+				return $id;	
 			}else{
 				return "Error";
 			}
